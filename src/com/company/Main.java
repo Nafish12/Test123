8cfb84c595ba1f908f34a5289731fff27b3ddaf3
md.nafisali <md.nafisali1@gmail.com>
2021-05-02 15:40:54 -0400

    My first commit on my own

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print.ln("hello people I do not know");
+    }
+}
+
