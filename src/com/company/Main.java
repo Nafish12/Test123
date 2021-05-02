4bef0e300f9fe5ee5e299a8cd29a5d4e12f48093
md.nafisali <md.nafisali1@gmail.com>
2021-05-02 16:53:33 -0400

    My second commit on my own

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.print.ln("hello people I do not know");
+        System.out.print.ln("hello people I hate");
     }
 }
 
